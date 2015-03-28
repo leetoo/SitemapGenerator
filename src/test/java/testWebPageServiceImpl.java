26f0463ec2b4293992e17e3d302c69fc5fe5b00b
sokyrka <sokyrka_evgeniy@yahoo.com>
2015-03-28 20:27:53 +0200

    added tests

--- src/test/java/testWebPageServiceImpl.java
+++ src/test/java/testWebPageServiceImpl.java
@@ -2,9 +2,12 @@ import com.uawebchallenge.service.WebPageService;
 import com.uawebchallenge.service.WebPageServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
 
 public class testWebPageServiceImpl {
 
@@ -31,11 +34,17 @@ public class testWebPageServiceImpl {
 
     @Test
     public void testCreateWebPages(){
-
+        WebPageServiceImpl wpsi = mock(WebPageServiceImpl.class);
+        wpsi.createWebPages("http://dou.ua");
+        InOrder inOrder = inOrder(wpsi);
+        inOrder.verify(wpsi).createWebPages("http://dou.ua");
     }
 
     @Test
     public void testCreateSiteMap(){
-
+        WebPageServiceImpl wpsi = mock(WebPageServiceImpl.class);
+        wpsi.createSiteMap("http://dou.ua");
+        InOrder inOrder = inOrder(wpsi);
+        inOrder.verify(wpsi).createSiteMap("http://dou.ua");
     }
 }
